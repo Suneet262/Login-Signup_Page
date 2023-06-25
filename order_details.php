1035d1f4f2db8a87da265bfefd37d28fa10b7a2d
Suneet Nankani <suneetnankani@gmail.com>
2023-06-25 12:16:00 +0530

    fetching data successfully without any js file just two file files extra: order_details.html and order_details.php

--- order_details.php
+++ order_details.php
@@ -15,8 +15,8 @@ if (!$connection) {
 }
 
 // Check if the name parameter is set in the URL
-if (isset($_GET['Customer_Name'])) {
-    $Customer_Name = $_GET['Customer_Name'];
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $Customer_Name = $_POST['Customer_Name'];
 
     // Construct the SQL query to fetch order details for the given client name
     $sql = "SELECT * FROM customers natural join product natural join orders natural join order_items
@@ -30,27 +30,33 @@ if (isset($_GET['Customer_Name'])) {
            // Start building the order details table
          // Start building the order details table
          $table = '<table class="order-table">';
-         $table .= '<tr><th>Order ID</th><th>Client Name</th><th>Product Type</th><th>Size</th><th>Color</th><th>Quantity</th></tr>';
+         $table .= '<tr><th>Order ID</th><th>Client Name</th><th>Client Email-Id</th><th>Product Type</th><th>Size</th><th>Color</th><th>Quantity</th><th>Order Date/Time</th><th>Delivery Address</th></tr>';
 
         // Fetch each row of the result set
         while ($row = mysqli_fetch_assoc($result)) {
             // Extract the order details from the row
             $Order_id = $row['Order_id'];
             $Customer_Name = $row['Customer_Name'];
+            $Customer_Email=$row['Customer_Email'];
             $Product_type = $row['Product_type'];
             $Size = $row['Size'];
             $Color = $row['Color'];
             $Quantity = $row['Quantity'];
+            $Order_date=$row['Order_date'];
+            $Delivery_Address=$row['Delivery_Address'];
 
             // Add a new row to the table for each order
             // Add a new row to the table for each order
             $table .= "<tr>
                         <td>$Order_id</td>
                         <td>$Customer_Name</td>
+                        <td>$Customer_Email</td>
                         <td>$Product_type</td>
                         <td>$Size</td>
                         <td>$Color</td>
                         <td>$Quantity</td>
+                        <td>$Order_date</td>
+                        <td>$Delivery_Address</td>
                     </tr>";
         }
 
