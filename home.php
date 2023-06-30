d4b2df09265c4c0c5b393df7ca16534dbff84b19
Suneet Nankani <suneetnankani@gmail.com>
2023-06-30 19:07:35 +0530

    succesfully logging in for both admin,user

--- home.php
+++ home.php
@@ -51,8 +51,9 @@
                 <h1>Welcome!! !!</h1>
                 <ul>
                     <li><a href="home.php">New Order</a></li>
-                    <li><a href="order_details.html">Search Order</a></li>
                     <li><a href="#">Track Order</a></li>
+                    <li><a href="order_details.html">Search Order</a></li>
+                    <!-- <li><a href="#">Track Order</a></li> -->
                     <li><a href="logout.php">Logout</a></li>
                 </ul>
             </div>
