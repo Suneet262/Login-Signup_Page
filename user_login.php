dd9e15f90af5814c6b52979881a1d3526bcfce63
Suneet Nankani <suneetnankani@gmail.com>
2023-06-30 17:47:59 +0530

    beginning of a new features

--- user_login.php
+++ user_login.php
@@ -1,18 +1,14 @@
 <?php
-session_start();
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve form data
     $email = $_POST["email"];
     $password = $_POST["password"];
-    //$message= "Login Successful";
     
     // Validate the form data (you can add more validation if required)
     if (empty($email) || empty($password)) {
         $error = "Please fill in all the fields.";
     } else {
         // TODO: Perform login and database operations
-        $_SESSION['email']= $email;
         
         // Assuming you have a database table named "users" with columns: id, name, email, password
         $conn = mysqli_connect('localhost', 'root', '','user_authentication_03');
@@ -35,30 +31,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $success = "Login successful!";
             header("Location: home.php");
             exit();
+            
         } else {
             // Invalid login credentials
             header("Location: login.html");
             // echo "Invalid Login Credentials.";
-           echo $error = "Invalid Login Credentials.";
-        //    alert("Invalid Login Credentials");
-        
-           
+            $error = "Invalid Login Credentials.";
         }
         
-
-        // if (isset($error)) {
-        //     echo "<div class='message error'>$error</div>";
-        // }
-
-        // if (isset($success)) {
-        //     echo "<div class='message success'>$success</div>";
-        // }
-
-        
-
         mysqli_close($conn);
     }
 }
 ?>
-
-<!-- Your login HTML code here -->
